fdc1dfd35a8e2ab86289296de708482328270da4
Sujin Jeon <mackerel0106@gmail.com>
2022-06-02 22:27:14 +0900

    0602: #3 종료

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-	title.style.color = "blue";
+	h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter() {
-	title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-	title.innerText = "Mouse is gone";
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleTitleClick);
