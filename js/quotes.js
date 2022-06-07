d9503e9b387bdb259339012e8fe207fb5408604b
Sujin Jeon <mackerel0106@gmail.com>
2022-06-08 00:38:21 +0900

    #6 진행중

---
+++ js/quotes.js
@@ -0,0 +1,57 @@
+const quotes = [
+	{
+		//1
+		quote: "Don’t dwell on the past.",
+		author: ""
+	},
+	{
+		//2
+		quote: "Carpe diem. Seize the day.",
+		author: "Dead Poets Society"
+	},
+	{
+		//3
+		quote: "Past is just past.",
+		author: ""
+	},
+	{
+		//4
+		quote: "Don’t beat yourself up.",
+		author: ""
+	},
+	{
+		//5
+		quote: "Where there is a will there is a way?",
+		author: "Angela Merkel"
+	},
+	{
+		//6
+		quote: "The die is cast.",
+		author: "Julius Caesar"
+	},
+	{
+		//7
+		quote: "I was never less alone than when by myself.",
+		author: "Edward Gibbon"
+	},
+	{
+		//8
+		quote: "Be brave.",
+		author: ""
+	},
+	{
+		//9
+		quote: "This is how life is.",
+		author: ""
+	},
+	{
+		//10
+		quote: "Respect individual.",
+		author: ""
+	}
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0]);
