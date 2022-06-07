75dc52044a9ce0930b45f10ff7d4d199bfd2adbc
Sujin Jeon <mackerel0106@gmail.com>
2022-06-07 21:10:10 +0900

    0607: #04 종료

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+// string을 중복적으로 작성할 때 오타가 생기는 걸 방지하기 위한 변수
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
 	event.preventDefault();
 	loginForm.classList.add("hidden");
-	const username = loginInput.value;
-	localStorage.setItem("username", username);
+	localStorage.setItem(USERNAME_KEY, loginInput.value);
+	paintGreetings();
+}
+
+function paintGreetings() {
+	const username = localStorage.getItem(USERNAME_KEY);
 	greeting.innerText = `Hello ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+	// show the form
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	// submit 이벤트가 일어났을 경우 진행
+	loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+	// show the greetings
+	paintGreetings();
+}
+
