b7f0d1bb244de8709dd4de37497b2faa7d1ed04f
Sujin Jeon <mackerel0106@gmail.com>
2022-07-14 00:29:43 +0900

    Fix: 파일 못 읽어오던것 해결

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from "express";
 import path from "path";
 
 const app = express();
+const dirName = path.resolve();
 
 app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => {
-	const dirName = path.resolve();
 	res.sendFile(path.join(dirName, '/index.html'));
 });
 app.listen(app.get('port'), () => {
 	// app.get('port') => 설정된 포트번호 반환
 	console.log(app.get('port'), 'Waiting...');
 });
+
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('img'));
