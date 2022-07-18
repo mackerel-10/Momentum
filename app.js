cc347afe75b9d1754b755a87b241f3becbdc1c06
Sujin Jeon <mackerel0106@gmail.com>
2022-07-18 21:17:48 +0900

    .env 사용 성공

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import path from "path";
+import dotenv from 'dotenv';
 
 const app = express();
 const dirName = path.resolve();
+
+dotenv.config();
 
 app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => {
@@ -14,5 +17,14 @@ app.listen(app.get('port'), () => {
 });
 
 app.use(express.static('css'));
-app.use(express.static('js'));
 app.use(express.static('img'));
+app.use(express.static('js'));
+
+const API_KEY = process.env.API_KEY;
+app.get('/get-weather-data', (req, res) => {
+	const { lat, lon } = req.query;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	fetch(url).then(response => response.json().then(data => {
+		res.send(data);
+	}));
+})
